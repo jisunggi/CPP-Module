3e88b7996bf520a563398603cb2ba2310bd640d0
jisunggi <wltjd5316@naver.com>
2023-07-29 01:30:35 +0900

    ex00 로직구현

--- cpp_module00/ex00/megaphone.cpp
+++ cpp_module00/ex00/megaphone.cpp
@@ -3,5 +3,17 @@
 int main (int argc, char **argv)
 {
 	if (argc < 2)
-		std::cout << "check argv"
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		return ;
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		for (int j = 0; argv[i][j]; j++)
+		{
+			argv[i][j] = toupper(argv[i][j]);
+			std::cout << argv[i][j];
+		}
+	}
+	std::cout << endl;
 }
