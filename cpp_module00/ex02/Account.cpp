167d479fdb8abc35ed6eb5b66da73b7b60e2211d
jisunggi <wltjd5316@naver.com>
2023-08-09 22:11:30 +0900

    ex02 tests.cpp Account.cpp

---
+++ cpp_module00/ex02/Account.cpp
@@ -0,0 +1,21 @@
+#include "Account.hpp"
+
+Account::Account(int initial_deposit)
+{
+	_amount = initial_deposit;
+	_accountIndex;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+
+	std::cout << ;
+}
+
+void	Account::displayAccountsInfos(void)
+{
+	std::cout << 
+}
+
+static void	Account::_displayTimestamp(void)
+{
+
+}
