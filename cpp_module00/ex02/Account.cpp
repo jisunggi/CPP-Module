a9308d76d6d7d4a94cf719a5c478823359473d9b
jisunggi <wltjd5316@naver.com>
2023-08-12 23:49:43 +0900

    makeDeposit 구현

--- cpp_module00/ex02/Account.cpp
+++ cpp_module00/ex02/Account.cpp
@@ -15,16 +15,74 @@ Account::Account(int initial_deposit)
 				<< std::endl;
 }
 
-static void	Account::_displayTimestamp(void)
+void	Account::_displayTimestamp(void)
 {
 	time_t timer = time(NULL);
 	struct tm* tm = localtime(&timer);
-	
+
 	char buffer[42];
 	strftime(buffer, 42, "%Y%m%D_%H%M%S", tm);
 }
 
 void	Account::displayAccountsInfos(void)
 {
-	std::cout << 
+	_displayTimestamp();
+	std::cout << " accounts:" << _nbAccounts << ";"
+			<< "total:" << _totalAmount << ";"
+			<< "deposits:" << _totalNbDeposits << ";"
+			<< "withdrawals:" << _totalNbWithdrawals << ";"
 }
+
+int	Account::getNbAccounts(void)
+{
+	return _nbAccounts;
+}
+
+int Account::getTotalAmount(void)
+{
+	return _totalAmount;
+}
+
+int Account::getNbDeposits(void)
+{
+	return _totalNbDeposits;
+}
+
+int Account::getNbWithdrawals(void)
+{
+	return _totalNbWithdrawals;
+}
+
+void	Account::displayStatus(void) const
+{
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex << ";"
+			<< "amount:" << _amount << ";"
+			<< "deposits:" << _nbDeposits << ";"
+			<< "withdrawals:" << _nbWithdrawals << ";"
+}
+
+// int	Account::checkAmount(void) const
+// {
+
+// }
+
+void	Account::makeDeposit(int deposit)
+{
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex << ";"
+			<< "p_amount:" << _amount << ";"
+			<< "deposit:" << deposit << ";"
+			<< "amount:" << _amount + deposit << ";"
+			<< "nb_deposits:" << _nbDeposits++ << ";"
+}
+
+// bool	Account::makeWithdrawal(int withdrawal)
+// {
+// 	_displayTimestamp();
+// 	std::cout << " index:" << _accountIndex << ";"
+// 			<< "p_amount:" << _amount << ";"
+// 			<< "deposit:" << deposit << ";"
+// 			<< "amount:" << _amount + deposit << ";"
+// 			<< "nb_deposits:" << _nbDeposits++ << ";"
+// }
