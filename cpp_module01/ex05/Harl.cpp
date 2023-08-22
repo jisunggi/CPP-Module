7797092cf43679b7893fb2a9ff8a80ebde051e63
jisunggi <wltjd5316@naver.com>
2023-08-23 05:33:20 +0900

    ex05 harl 오타 수정

--- cpp_module01/ex05/Harl.cpp
+++ cpp_module01/ex05/Harl.cpp
@@ -11,14 +11,12 @@ void Harl::debug(void)
 
 void Harl::info(void)
 {
-    std::cout << "I cannot believe adding extra bacon costs more money. You didn¡¯t put
-        enough bacon in my burger! If you did, I wouldn¡¯t be asking for more!" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn¡¯t put enough bacon in my burger! If you did, I wouldn¡¯t be asking for more!" << std::endl;
 }
 
 void Harl::warning(void)
 {
-    std::cout << "I think I deserve to have some extra bacon for free. I¡¯ve been coming for
-        years whereas you started working here since last month." << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free. I¡¯ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void Harl::error(void)
