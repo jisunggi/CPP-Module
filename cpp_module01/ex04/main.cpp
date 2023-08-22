29675916a64654f6260f5ec1b85cd696bd94f410
jisunggi <wltjd5316@naver.com>
2023-08-23 04:20:08 +0900

    ex04 replace 구현

---
+++ cpp_module01/ex04/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+
+int check_error(std::ifstream infile, std::ofstream outfile, std::string string1, std::string string2)
+{
+    if (!infile.is_open() || !outfile.is_open())
+    {
+        std::cout << "file didn't open properly" << std::endl;
+        return 1;
+    }
+    if (string1.length() == 0 || string2.length() == 0)
+    {
+        std::cout << "no string" << std::endl;
+        return 1;
+    }
+}
+
+void    ft_replace(std::string &line, std::string string1, std::string string2)
+{
+    size_t pos = 0;
+
+    pos = line.find(string1, pos);
+    while (pos != std::string::npos)
+    {
+        line.erase(pos, string1.length());
+        line.insert(pos, string2.length());
+        pos += string2.length();
+        pos = line.find(string1, pos);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 4)
+    {
+        std::cout << "argc error" << std::endl;
+        return 1;
+    }
+
+    std::ifstream infile(argv[1]);
+    std::string newFilename = std::string(argv[1]) + ".replace";
+    std::ofstream outfile(newFilename);
+    std::string string1;
+    std::string string2;
+    std::string line;
+
+    string1 = argv[2];
+    string2 = argv[3];
+
+    if (check_error(infile, outfile, string1, string2))
+        return 1;
+    
+    while (!infile.eof())
+    {
+        std::getline(infile, line);
+        ft_replace(line, string1, string2);
+        outfile << line;
+        if (!infile.eof())
+            outfile << std::endl;
+    }
+}
