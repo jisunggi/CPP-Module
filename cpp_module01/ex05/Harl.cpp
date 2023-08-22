f2b61963f2c3c7d8338e2d73e3b7245e6fe86c35
jisunggi <wltjd5316@naver.com>
2023-08-23 05:34:20 +0900

    ex05 harl 오타 수정

--- cpp_module01/ex05/Harl.cpp
+++ cpp_module01/ex05/Harl.cpp
@@ -27,7 +27,7 @@ void Harl::error(void)
 void Harl::complain(std::string level)
 {
     std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-    void (Harl::*f[4])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error}
+    void (Harl::*f[4])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
 
     for(int i = 0; i < 4; i++)
     {
