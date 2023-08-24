d806987082bb8025978dd310fb0e4fc20b83291f
jisunggi <wltjd5316@naver.com>
2023-08-25 04:28:56 +0900

    ex02 후위연산자 수정

--- cpp_module02/ex02/Fixed.cpp
+++ cpp_module02/ex02/Fixed.cpp
@@ -3,30 +3,30 @@
 Fixed::Fixed()
 {
     fixValue = 0;
-    std::cout << "Default constructor called" << std::endl;
+    //std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(const int value)
 {
     this->fixValue = value << bit;
-    std::cout << "Int constructor called" << std::endl;
+    //std::cout << "Int constructor called" << std::endl;
 }
 
 Fixed::Fixed(const float value)
 {
     this->fixValue = roundf(value * (1 << this->bit));
-    std::cout << "Float constructor called" << std::endl;
+    //std::cout << "Float constructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed &fixed)
 {
-    std::cout << "Copy constructor called" << std::endl;
+    //std::cout << "Copy constructor called" << std::endl;
     this->fixValue = fixed.getRawBits();
 }
 
 Fixed::~Fixed()
 {
-    std::cout << "Destructor called" << std::endl;
+    //std::cout << "Destructor called" << std::endl;
 }
 
 float Fixed::toFloat() const
@@ -41,7 +41,7 @@ int Fixed::toInt() const
 
 Fixed &Fixed::operator=(const Fixed &fixed)
 {
-    std::cout << "Copy assignment operator called" << std::endl;
+    //std::cout << "Copy assignment operator called" << std::endl;
     if (this != &fixed)
         this->fixValue = fixed.getRawBits();
     return *this;
