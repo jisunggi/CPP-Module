685c2822e4dafece6382910719ec764a7828eb23
jisunggi <wltjd5316@naver.com>
2023-09-04 00:37:51 +0900

    ex04 strart

---
+++ cpp_module04/ex03/AMateria.hpp
@@ -0,0 +1,19 @@
+#ifndef AMATERIA_HPP
+# define AMATERIA_HPP
+
+#include <iostream>
+#include <string>
+
+class AMateria
+{
+    protected:
+        std::string type;
+
+    public:
+        AMateria(std::string const & type);
+        std::string const & getType() const; //Returns the materia type
+        virtual AMateria* clone() const = 0;
+        virtual void use(ICharacter& target);
+};
+
+#endif
