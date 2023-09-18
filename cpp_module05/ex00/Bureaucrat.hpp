f89530623b8f70b38065ace86edfe32a72a04284
jisunggi <wltjd5316@naver.com>
2023-09-18 23:00:02 +0900

    cpp_module05 ex01 start

---
+++ cpp_module05/ex00/Bureaucrat.hpp
@@ -0,0 +1,16 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <string>
+#include <iostream>
+
+class Bureaucrat
+{
+	public :
+		std::string name;
+		unsigned int grade;
+
+};
+
+
+#endif
