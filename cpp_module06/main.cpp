152c125e17e4b5a35ed2499a9b91ae16683c49e2
jisunggi <wltjd5316@naver.com>
2023-10-15 02:10:37 +0900

    확인

--- cpp_module06/main.cpp
+++ cpp_module06/main.cpp
@@ -1,46 +1,5 @@
 #include "ScalarConverter.hpp"
 
-int	ScalarConverter::stringToInt(std::string string)
-{
-	std::stringstream stream;
-	int integer;
-
-	stream << string;
-	stream >> integer;
-	if (stream.fail())
-	{
-		dataType = "double";
-		error = 1;
-	}
-	return integer;
-}
-
-float ScalarConverter::stringToFloat(std::string string)
-{
-	std::stringstream stream;
-	float rtnFloat;
-
-	stream << string;
-	stream >> rtnFloat;
-	if (stream.fail())
-		return error = 1;
-	std::cout << rtnFloat << std::endl;
-	return rtnFloat;
-}
-
-double ScalarConverter::stringToDouble(std::string string)
-{
-	std::stringstream stream;
-	double rtnDouble;
-
-	stream << string;
-	stream >> rtnDouble;
-	if (stream.fail())
-		return error = 1;
-	return rtnDouble;
-}
-
-
 int main (int argc, char **argv)
 {
 	if (argc < 2)
