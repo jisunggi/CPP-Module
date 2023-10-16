5a9c13eeab6a6f69d5f7b2948d250422a7694103
jisunggi <wltjd5316@naver.com>
2023-10-17 02:47:31 +0900

    ex02 복사 생성자 수정

--- cpp_module07/ex02/Array.hpp
+++ cpp_module07/ex02/Array.hpp
@@ -34,7 +34,7 @@ class Array
 				delete array;
 			array = new T[n];
 			for (unsigned int i = 0; i < n; i++)
-        		array[i] = object[i];
+        		array[i] = object.array[i];
     	};
 		Array &operator=(const Array &object)
 		{
@@ -45,8 +45,7 @@ class Array
 					delete array;
 				array = new T[n];
 				for (unsigned int i = 0; i < n; i++)
-        			array[i] = object[i];
-
+        			array[i] = object.array[i];
 			}
 			return *this;
 		};
