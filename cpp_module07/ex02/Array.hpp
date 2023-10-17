44329aaa4d55d281b723b9be1d724f8cc57ebab2
jisunggi <wltjd5316@naver.com>
2023-10-18 03:16:44 +0900

    확인

--- cpp_module07/ex02/Array.hpp
+++ cpp_module07/ex02/Array.hpp
@@ -32,10 +32,12 @@ class Array
 		};
 		Array(const Array &object)
 		{
+			/*
 			this->n = object.n;
 			array = new T[n];
 			for (unsigned int i = 0; i < n; i++)
         		array[i] = object.array[i];
+				*/
 			*this = object;
 		};
 		Array &operator=(const Array &object)
