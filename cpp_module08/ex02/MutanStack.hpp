c707205bf280364b96ecb649e5ddd1535a1e548b
jisunggi <wltjd5316@naver.com>
2023-10-23 01:36:05 +0900

    ex02 구현

---
+++ cpp_module08/ex02/MutanStack.hpp
@@ -0,0 +1,46 @@
+#ifndef MUTANSTACK_HPP
+# define MUTANSTACK_HPP
+
+template <typename T>
+class MutanStack : public std::stack<>
+{
+	public :
+		MutanStack()
+		{};
+		~MutanStack()
+		{};
+		MutanStack(const MutanStack &object)
+		{
+            *this = object;
+		};
+		MutanStack &operator=(const MutanStack &object)
+		{
+			if (this != &object)
+			{
+                *this = object;
+			}
+			return *this;
+        }
+
+        typedef typename MutanStack<T>::stack::container_type::iterator iterator;
+        iterator begin()
+        {
+            return c.begin();
+        }
+        iterator end()
+        {
+            return c.end();
+        }
+
+        typedef typename MutanStack<T>::stack::container_type::reverse_iterator reverse_iterator;
+        reverse_iterator rbegin()
+        {
+            return c.rbegin();
+        }
+        reverse_iterator rend()
+        {
+            return c.rend();
+        }
+}
+
+#endif
