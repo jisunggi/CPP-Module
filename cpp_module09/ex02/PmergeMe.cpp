536b708a551b87fa4ffffed4f3334f8d10ba6c71
jisunggi <wltjd5316@naver.com>
2023-10-29 00:09:17 +0900

    확인

--- cpp_module09/ex02/PmergeMe.cpp
+++ cpp_module09/ex02/PmergeMe.cpp
@@ -27,35 +27,6 @@ PmergeMe &PmergeMe::operator=(const PmergeMe &PmergeMe)
 // 	if (left < right)
 // }
 
-// Binary search for insertion
-int PmergeMe::binarySearch(const std::vector<int>& arr, int left, int right, int key) {
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] == key) {
-            return mid;
-        }
-        if (arr[mid] < key) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-    return left;
-}
-
-// Insertion sort with binary search for the "main Chain"
-void PmergeMe::insertionSort(std::vector<int>& arr) {
-    int n = arr.size();
-    for (int i = 1; i < n; i++) {
-        int key = arr[i];
-        int j = i - 1;
-        while (j >= 0 && arr[j] > key) { // Change the comparison to ascending order
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = key;
-    }
-}
 
 
 
@@ -104,33 +75,28 @@ void PmergeMe::makeMainChain()
 
 }
 
+int insert_element(const std::vector<int>& mainChain, int element) {
+    int left = 0;
+    int right = mainChain.size() - 1;
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        if (element < mainChain[mid]) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left;
+}
+
 void PmergeMe::mergeInsertionSort()
 {
 	//mergeSort(0, inputData.size() - 1);
 	makeMainChain();
 	//insertionSort();
-	insertionSort(otherChain); // Step 4: Insertion Sort for other Chain
-
-    // Merge the main and other chains
-    std::size_t i = 0, j = 0;
-    while (i < mainChain.size() && j < otherChain.size()) {
-        if (mainChain[i] >= otherChain[j]) {
-            inputData[i + j] = mainChain[i];
-            i++;
-        } else {
-            inputData[i + j] = otherChain[j];
-            j++;
-        }
-    }
-
-    while (i < mainChain.size()) {
-        inputData[i + j] = mainChain[i];
-        i++;
-    }
-
-    while (j < otherChain.size()) {
-        inputData[i + j] = otherChain[j];
-        j++;
+	for (int element : pendingElements) {
+        int index = insert_element(mainChain, element);
+        mainChain.insert(mainChain.begin() + index, element);
     }
 }
 
