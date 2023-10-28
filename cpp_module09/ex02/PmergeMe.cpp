942166abe9deb81d7607e2d788cf45d76f63086e
jisunggi <wltjd5316@naver.com>
2023-10-28 20:13:18 +0900

    mainChain 확인

--- cpp_module09/ex02/PmergeMe.cpp
+++ cpp_module09/ex02/PmergeMe.cpp
@@ -29,7 +29,7 @@ PmergeMe &PmergeMe::operator=(const PmergeMe &PmergeMe)
 
 void PmergeMe::makeMainChain()
 {
-	for (int i = 0; i < inputData.size(); i += 2)
+	for (std::size_t i = 0; i < inputData.size(); i += 2)
 	{
 		if (inputData[i] > inputData[i + 1])
 		{
