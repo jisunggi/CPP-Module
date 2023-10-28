3fc946b0257603687f2bf11f8b95914b0d98b3e6
jisunggi <wltjd5316@naver.com>
2023-10-29 05:01:34 +0900

    deque 추가

--- cpp_module09/ex02/PmergeMe.cpp
+++ cpp_module09/ex02/PmergeMe.cpp
@@ -91,7 +91,7 @@ void PmergeMe::makeMainChain()
 	mergeSort(0, mainChain.size() - 1);
 }
 
-int PmergeMe::insert_element(int element)
+int PmergeMe::insertElement(int element)
 {
     int left = 0;
     int right = mainChain.size() - 1;
@@ -121,7 +121,7 @@ void PmergeMe::insertionSort()
 	std::size_t prevJacobsthal = 0;
 	int n = 0;
 
-	int index = insert_element(otherChain[0]);
+	int index = insertElement(otherChain[0]);
 	mainChain.insert(mainChain.begin() + index, otherChain[0]);
 	numbersOfinsert++;
 	while (1)
@@ -160,15 +160,145 @@ void PmergeMe::mergeInsertionSort()
 	insertionSort();
 }
 
+void PmergeMe::dequeMerge(int left, int mid, int right) 
+{
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    std::vector<int> leftArray(n1);
+    std::vector<int> rightArray(n2);
+    for (int i = 0; i < n1; i++) 
+        leftArray[i] = mainChainDeque[left + i];
+    for (int i = 0; i < n2; i++)
+        rightArray[i] = mainChainDeque[mid + 1 + i];
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) 
+	{
+        if (leftArray[i] <= rightArray[j]) 
+		{
+            mainChainDeque[k] = leftArray[i];
+            i++;
+        }
+		else 
+		{
+            mainChainDeque[k] = rightArray[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1) 
+	{
+        mainChainDeque[k] = leftArray[i];
+        i++;
+        k++;
+    }
+    while (j < n2) 
+	{
+        mainChainDeque[k] = rightArray[j];
+        j++;
+        k++;
+    }
+}
+
+void PmergeMe::dequeMergeSort(int left, int right) 
+{
+	if (left < right) 
+	{
+        int mid = left + (right - left) / 2;
+
+        dequeMergeSort(left, mid);
+        dequeMergeSort(mid + 1, right);
+        dequeMerge(left, mid, right);
+    }
+}
+
+void PmergeMe::dequeMakemainChain()
+{
+	std::size_t i;
+	for (i = 0; i < inputData.size(); i += 2)
+	{
+		std::cout << inputData[i] << std::endl;
+		if (!inputData[i + 1])
+		{
+			otherChainDeque.push_back(inputData[i]);
+			break;
+		}
+		mainChainDeque.push_back(inputData[i]);
+		otherChainDeque.push_back(inputData[i + 1]);
+	}
+	dequeMergeSort(0, mainChainDeque.size() - 1);
+}
+
+int PmergeMe::dequeInsertElement(int element)
+{
+    int left = 0;
+    int right = mainChainDeque.size() - 1;
+    while (left <= right)
+	{
+        int mid = (left + right) / 2;
+        if (element < mainChainDeque[mid])
+            right = mid - 1;
+		else
+            left = mid + 1;
+    }
+    return left;
+}
+
+void PmergeMe::dequeInsertionSort()
+{
+	std::size_t numbersOfinsert = 0;
+	std::size_t jacobsthalNum = 0;
+	std::size_t prevJacobsthal = 0;
+	int n = 0;
+
+	int index = dequeInsertElement(otherChainDeque[0]);
+	mainChainDeque.insert(mainChainDeque.begin() + index, otherChainDeque[0]);
+	numbersOfinsert++;
+	while (1)
+	{
+		prevJacobsthal = jacobsthalNum;
+		jacobsthalNum = jacobsthal(n);
+
+		std::cout << "prev : " << prevJacobsthal << std::endl;
+		std::cout << "jacobsthalNum : " << jacobsthalNum << std::endl;
+
+		for (std::size_t i = jacobsthalNum; i != prevJacobsthal; i--)
+		{
+			if (numbersOfinsert >= otherChainDeque.size())
+				break ;
+			if (i > otherChainDeque.size() - 1)
+				i = otherChainDeque.size() - 1;
+			std::cout << "index : " << i << std::endl;
+
+			index = dequeInsertElement(otherChainDeque[i]);
+        	mainChainDeque.insert(mainChainDeque.begin() + index, otherChainDeque[i]);
+			
+			std::cout << otherChainDeque[i] << std::endl;
+			std::cout << "numbersof : " << numbersOfinsert << std::endl;
+
+			numbersOfinsert++;
+		}
+		if (numbersOfinsert >= otherChainDeque.size())
+			break ;
+		n++;
+	}
+}
+
+void PmergeMe::dequeMergeInsertionSort()
+{
+	dequeMakemainChain();
+	dequeInsertionSort();
+}
+
 void PmergeMe::printsortedValue()
 {
 	std::cout << "mainChain : ";
-	for (std::size_t i = 0; i < mainChain.size(); i++)
-		std::cout << mainChain[i] << " ";
+	for (std::size_t i = 0; i < mainChainDeque.size(); i++)
+		std::cout << mainChainDeque[i] << " ";
 	std::cout << std::endl;
 	std::cout << "otherChain : ";
-	for (std::size_t i = 0; i < otherChain.size(); i++)
-		std::cout << otherChain[i] << " ";
+	for (std::size_t i = 0; i < otherChainDeque.size(); i++)
+		std::cout << otherChainDeque[i] << " ";
 	std::cout << std::endl;
 }
 
@@ -183,22 +313,22 @@ void PmergeMe::vectorSort()
 	clock_t start = clock();
 	mergeInsertionSort();
 	clock_t end = clock();
-	printsortedValue();
+	//printsortedValue();
 	std::cout << "Time to process a range of " << std::setw(4) << mainChain.size() << " elements with std::vector : " << end - start << "ms" << std::endl;
 }
 
-// void PmergeMe::deaueSort()
-// {
-// 	clock_t start = clock();
-// 	mergeInsertionSort();
-// 	clock_t end = clock();
-// 	printsortedValue();
-// 	std::cout << "Time to process a range of " << std::setw(4) << this->vectorData.size() << " elements with std::vector : " << end - start << "ms" << std::endl;
-// }
+void PmergeMe::deaueSort()
+{
+	clock_t start = clock();
+	mergeInsertionSort();
+	clock_t end = clock();
+	printsortedValue();
+	std::cout << "Time to process a range of " << std::setw(4) << mainChainDeque.size() << " elements with std::deaue : " << end - start << "ms" << std::endl;
+}
 
 void PmergeMe::executePmergeMe()
 {
 	printInputDataValue();
 	vectorSort();
-	//dequeSort();
+	dequeSort();
 }
--- cpp_module09/ex02/PmergeMe.hpp
+++ cpp_module09/ex02/PmergeMe.hpp
@@ -16,8 +16,8 @@ class PmergeMe
 		std::vector<int> inputData;
 		std::vector<int> mainChain;
 		std::vector<int> otherChain;
-		//std::deque<int> mainChainDeque;
-		//std::deque<int> otherChainDeque;
+		std::deque<int> mainChainDeque;
+		std::deque<int> otherChainDeque;
 
     public :
         PmergeMe(std::vector<int> inputData);
@@ -29,6 +29,7 @@ class PmergeMe
 		void printInputDataValue();
 
 		void executePmergeMe();
+		
 		void vectorSort();
 		void mergeInsertionSort();
 		void makeMainChain();
@@ -36,11 +37,20 @@ class PmergeMe
 		void merge(int left, int mid, int right);
 		void mergeSort(int left, int right);
 
-		int insert_element(int element);
+		int insertElement(int element);
 		void insertionSort();
 		int jacobsthal(int n);
 
 		void deaueSort();
+		void dequeMergeInsertionSort();
+		void dequeMakemainChain();
+
+		void dequeMergeSort(int left, int right);
+		void dequeMerge(int left, int mid, int right);
+
+		int dequeInsertElement(int element);
+		void dequeInsertionSort();
+
 };
 
 #endif
